f4351397d3f32b8106f39a0bfe654ac47ee44d17
Sowmya <chsowmya6@gmail.com>
2021-09-25 21:04:41 +0530

    Initial commit

---
+++ InfyGo-Flight/src/main/java/com/infosys/irs/flight/InfyGoFlightApplication.java
@@ -0,0 +1,13 @@
+package com.infosys.irs.flight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfyGoFlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfyGoFlightApplication.class, args);
+	}
+
+}
---
+++ InfyGo-Flight/src/test/java/com/infosys/irs/flight/InfyGoFlightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosys.irs.flight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfyGoFlightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
